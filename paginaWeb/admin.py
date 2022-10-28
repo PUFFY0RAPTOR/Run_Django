5a349946737c70ece817369fde2944caeffd53f4
mateo <mateo.ortiz54@misena.edu.co>
2022-10-28 07:13:32 -0500

    Confirmando cambios

---
+++ paginaWeb/admin.py
@@ -0,0 +1,94 @@
+from django.contrib import admin
+from .models import Roles, Marcas, Usuarios, Empleados, Clientes, Pedidos, Marcas, Productos, PedidosProductos, Envios, Ventas, Historial, MediosDePagos, Pagos
+# Register your models here.
+
+@admin.register(Roles)
+class RolesAdmin(admin.ModelAdmin):
+    list_display = ('id_roles', 'nombre_rol')
+    search_fields = ['id_roles', 'nombre_rol']
+    
+""" @admin.register(Permisos)
+class PermisosAdmin(admin.ModelAdmin):
+    list_display = ('nombre_permiso', 'description')
+    search_fields = ['nombre_permiso', 'description']    """
+    
+@admin.register(Marcas)
+class MarcasAdmin(admin.ModelAdmin):
+    list_display = ('id_marca','nombre_marca',)
+    search_fields = ['id_marca', 'nombre_marca',]
+
+""" @admin.register(RolesPermisos)
+class RolesPermisosAdmin(admin.ModelAdmin):
+    list_display = ('id_roles_permisos','roles','permisos',)
+    search_fields = ['id_roles_permisos',] """
+
+@admin.register(Usuarios)
+class UsuariosAdmin(admin.ModelAdmin):
+    list_display = ('id_correo','contrasena', 'roles')
+    search_fields = ['id_correo', 'roles__roles']
+
+@admin.register(Empleados)
+class EmpleadosAdmin(admin.ModelAdmin):
+    list_display = ('id_empleado','nombre_empleado', 'apellido_empleado', 'celular_empleado', 'fecha_nacimiento', 'eps', 'Correo_Usuario')
+    search_fields = ['id_empleado', 'nombre_empleado', 'apellido_empleado', 'celular_empleado', 'fecha_nacimiento', 'eps', 'usuarios__correo']
+
+    def Correo_Usuario(obj, self):
+        return self.correo.id_correo
+
+@admin.register(Clientes)
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ('id_cliente','nombre_cliente', 'apellido_cliente', 'celular_cliente', 'fecha_nacimiento', 'direccion', 'Correo_Usuario')
+    search_fields = ['id_cliente', 'nombre_cliente', 'apellido_cliente', 'celular_cliente', 'fecha_nacimiento', 'direccion', 'usuarios__correo']
+
+    def Correo_Usuario(obj, self):
+        return self.correo.id_correo
+
+@admin.register(Pedidos)
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ('id_pedido','cliente')
+    search_fields = ['id_pedido','cliente__cliente']
+
+@admin.register(Productos)
+class ProductosAdmin(admin.ModelAdmin):
+    list_display = ('id_producto','nombre_producto', 'stock', 'precio', 'marca', 'verImagen')
+    search_fields = ['id_producto','nombre_producto', 'stock', 'precio', 'marca']
+    def verImagen(self, obj):
+        #general codigo html en el admin
+        from django.utils.html import format_html
+        imagen = obj.imagen.url
+        return format_html(
+            f"<a href='{imagen}' target='_blank' ><img src='{imagen}'  width='20%' /></a>"
+        )
+
+@admin.register(PedidosProductos)
+class PedidosProductosAdmin(admin.ModelAdmin):
+    list_display = ('id_pedidos_productos','pedido', 'producto')
+    search_fields = ['id_pedidos_productos','pedido__pedido', 'producto_producto']
+
+@admin.register(Envios)
+class EnviosAdmin(admin.ModelAdmin):
+    list_display = ('id_envios','fecha_envio', 'direccion_envio')
+    search_fields = ['id_envios','fecha_envio', 'direccion_envio']
+
+@admin.register(Ventas)
+class VentasAdmin(admin.ModelAdmin):
+    list_display = ('id_venta','pedido', 'envio', 'fecha')
+    search_fields = ['id_venta','pedido_pedido', 'envio_envio', 'fecha']
+
+@admin.register(MediosDePagos)
+class MediosDePagosAdmin(admin.ModelAdmin):
+    list_display = ('id_medio_pago','nombre_medio_pago', 'estado_medio_pago')
+    search_fields = ['id_medio_pago','nombre_medio_pago', 'estado_medio_pago']
+
+@admin.register(Pagos)
+class PagosAdmin(admin.ModelAdmin):
+    list_display = ('id_pago','medio_pago', 'fecha_pagos')
+    search_fields = ['id_pago','medio_pago', 'fecha_pagos']
+
+@admin.register(Historial)
+class HistorialAdmin(admin.ModelAdmin):
+    list_display = ('id_historial','venta', 'pago')
+    search_fields = ['id_historial','venta', 'pago']
+
+"""
+"""
---
+++ paginaWeb/models.py
@@ -0,0 +1,148 @@
+from django.db import models
+
+# Create your models here.
+
+class Roles(models.Model):
+    id_roles = models.IntegerField(primary_key=True)
+    nombre_rol = models.CharField(max_length=100)
+    descripcion = models.TextField(default="Descripción del rol...")
+    
+    def __str__(self):
+        return self.nombre_rol
+
+class Marcas(models.Model):
+    id_marca = models.IntegerField(primary_key=True)
+    nombre_marca = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre_marca
+       
+""" class Permisos(models.Model):
+    id_permiso = models.IntegerField(primary_key=True)
+    nombre_permiso = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    #intermedia = models.ManyToManyField(Roles,blank=True)
+
+    def __str__(self):
+        return self.nombre_permiso
+
+class RolesPermisos(models.Model):
+    id_roles_permisos = models.IntegerField(primary_key=True)
+    roles = models.ForeignKey(Roles, on_delete = models.DO_NOTHING)
+    permisos = models.ForeignKey(Permisos, on_delete = models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nombre_rol """
+
+
+class Usuarios(models.Model):
+    id_correo = models.CharField(max_length=150, primary_key=True)
+    contrasena = models.CharField(max_length=14)
+    roles = models.ForeignKey(Roles, on_delete = models.DO_NOTHING)
+
+    def __str__(self):
+        return self.id_correo
+
+
+class Empleados(models.Model):
+    id_empleado = models.IntegerField(primary_key=True)
+    nombre_empleado = models.CharField(max_length=100)
+    apellido_empleado = models.CharField(max_length=100)
+    celular_empleado = models.IntegerField()
+    fecha_nacimiento = models.DateField(default='2000-01-01')
+    direccion_empleado = models.CharField(max_length=100, default="Cerca de la empresa")
+    eps = models.CharField(max_length=50)
+    correo = models.ForeignKey(Usuarios, on_delete = models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nombre_empleado
+
+class Clientes(models.Model):
+    id_cliente = models.IntegerField(primary_key=True)
+    nombre_cliente = models.CharField(max_length=100)
+    apellido_cliente = models.CharField(max_length=100)
+    celular_cliente = models.IntegerField()
+    fecha_nacimiento = models.DateField(default='2000-01-01')
+    direccion = models.CharField(max_length=125)
+    correo = models.ForeignKey(Usuarios, on_delete = models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nombre_cliente
+
+class Pedidos(models.Model):
+    id_pedido = models.IntegerField(primary_key=True)
+    cliente = models.ForeignKey(Clientes, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return "Pedidos"  #what?
+
+class Marcas(models.Model):
+    id_marca = models.IntegerField(primary_key=True)
+    nombre_marca = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_marca
+
+class Productos(models.Model):
+    id_producto = models.IntegerField(primary_key=True)
+    nombre_producto = models.CharField(max_length=100)
+    stock = models.IntegerField()
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    marca = models.ForeignKey(Marcas, on_delete = models.DO_NOTHING)
+    descripcion = models.TextField(default="")
+    imagen = models.ImageField(upload_to = 'RUN/imagProductos', default='RUN/imagProductos/default.png')
+
+    def __str__(self):
+        return self.nombre_producto
+
+class PedidosProductos(models.Model):
+    id_pedidos_productos = models.IntegerField(primary_key=True)
+    pedido = models.ForeignKey(Pedidos, on_delete=models.DO_NOTHING)
+    producto = models.ForeignKey(Productos, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return "Pedidos Productos"
+
+class Envios(models.Model):
+    id_envios = models.IntegerField(primary_key=True)
+    fecha_envio = models.DateField(default="2000-01-01")
+    direccion_envio = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.direccion_envio
+
+class Ventas(models.Model):
+    id_venta = models.IntegerField(primary_key=True)
+    pedido = models.ForeignKey(Pedidos, on_delete=models.DO_NOTHING)
+    envio = models.ForeignKey(Envios, on_delete=models.DO_NOTHING)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Ventas"
+
+class MediosDePagos(models.Model):
+    id_medio_pago = models.IntegerField(primary_key=True)
+    nombre_medio_pago = models.CharField(max_length=50)
+    estado_medio_pago = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.nombre_medio_pago
+
+class Pagos(models.Model):
+    id_pago = models.IntegerField(primary_key=True)
+    medio_pago = models.ForeignKey(MediosDePagos, on_delete=models.DO_NOTHING)
+    fecha_pagos = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.fecha_pagos)
+
+class Historial(models.Model):
+    id_historial = models.IntegerField(primary_key=True)
+    venta = models.ForeignKey(Ventas, on_delete=models.DO_NOTHING)
+    pago = models.ForeignKey(Pagos, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return "Historial"
+
+"""          
+"""
